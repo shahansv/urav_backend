9a2d082af3e7d60bc0f6021bdcaa9d1d2e7e7d3f
shahansv <shahanvsaleem@gmail.com>
2026-03-26 23:18:11 +0530

    bug fix

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,8 @@ const ideaModel = require("../models/ideaModel");
 
 exports.getIdeas = async (req, res) => {
   try {
-    let allIdeas = await ideaModel.find();
+    let allIdeas = await ideaModel.find().sort({ createdAt: -1 });
+
     res.status(200).json({ message: "All ideas fetched", allIdeas });
   } catch (error) {
     console.log(error);
